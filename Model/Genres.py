90e5ddc129c040dfb8f6800e8b1286a11196e38a
Diego <48367473+Itexe2B@users.noreply.github.com>
2022-12-15 13:53:23 +0100

    Get list genre

--- Model/Genres.py
+++ Model/Genres.py
@@ -3,6 +3,15 @@ import pandas as pd
 from Data import Data
 
 class Genres:
-    data = Data.get_instance()
     def __init__(self):
-        pass
+        self.data = Data.get_instance()
+
+    def get_list_genres(self):
+        list_genre = []
+        id_list_exclude = []
+        for genres in self.data.df_movies['genres']:
+            for genre in genres:
+                if genre['id'] not in id_list_exclude:
+                    id_list_exclude.append(genre['id'])
+                    list_genre.append(genre)
+        return list_genre
